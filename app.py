3b5a95b61893ecdd82e8887be25185384c1d7af4
enrique-zarate <eszg.fury@gmail.com>
2022-11-15 11:44:15 -0300

    se agregan listado de pacientes y perfil del paciente

--- app.py
+++ app.py
@@ -1,17 +1,34 @@
-from flask import Flask
-# import libraries
-from flask import render_template
+from flask import Flask, render_template
+# import sqlalchemy
 from flask_sqlalchemy import SQLAlchemy
 # import pycopg2-binary
 import psycopg2
 # importar los modelos de la base de datos
 from models import Paciente
+# import os
+import os
 
 # crear la aplicacion
 app = Flask(__name__)
 
 # creamos objeto de base de datos
+
+# configurar la base de datos
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://enri:kike2311@localhost:5432/flask_db'
+# desactivar el track de modificaciones
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# inicializar la base de datos
 db = SQLAlchemy(app)
+db.init_app(app)
+
+# create object connection
+conn = psycopg2.connect(
+        host="localhost",
+        database="flask_db",
+        user=os.environ['DB_USERNAME'],
+        password=os.environ['DB_PASSWORD'])
+        
+
 
 # # crear una conexion a la base de datos postgres
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/flask_db'
@@ -20,15 +37,16 @@ db = SQLAlchemy(app)
 # # inicializar la base de datos
 # db.init_app(app)
 
-def connection():
-    s = 'localhost:5432/flask_db' #Your server(host) name 
-    d = 'patients' #Your database name
-    u = 'enri' #Your login user
-    p = 'kike2311' #Your login password
-    conn = psycopg2.connect(host=s, user=u, password=p, database=d)
-    return conn
+# def connection():
+#     s = 'localhost:5432/flask_db' #Your server(host) name 
+#     d = 'patients' #Your database name
+#     u = 'enri' #Your login user
+#     p = 'kike2311' #Your login password
+#     conn = psycopg2.connect(host=s, user=u, password=p, database=d)
+#     return conn
     
-conn = connection()
+# conn = connection()
+
 
 
 # # create postgres tables
@@ -40,19 +58,46 @@ conn = connection()
 def index():
     return {'Saludo' : 'Hola Mundo'}
 
+
+
 # crear ruta para mostrar los pacientes
 @app.route('/patients')
 def patients():
+    # create a connection to the database and the user data
+    conn = psycopg2.connect(
+        host="localhost",
+        database="flask_db",
+        user=os.environ['DB_USERNAME'],
+        password=os.environ['DB_PASSWORD'])
     # crear una lista de pacientes
     patients = []
     # crear conexion a la base de datos y los datos del usuario
-    conn = psycopg2.connect("dbname=flask_db user=enri")
+    # conn = psycopg2.connect("dbname=flask_db user=enri")
     # crear cursor
     cur = conn.cursor()
     # ejecutar query
     cur.execute("SELECT * FROM patients")
     # fetch de los datos a la tabla
     for row in cur.fetchall():
-        patients.append({"id": row[0], "name": row[1], "year": row[2], "price": row[3]})
+        patients.append({"id": row[0], "nombre": row[1], "fecha_nacimiento": row[2], "signos_vitales": row[3]})
     conn.close()
-    return render_template('patients.html', patients=patients)
+    return render_template('patients_list.html', patients=patients)
+
+    # create an endpoint to edit a patient
+@app.route('/patients/<int:id>' , methods=['GET', 'POST'])
+def patient(id):
+    # create a connection to the database and the user data
+    conn = psycopg2.connect(
+        host="localhost",
+        database="flask_db",
+        user=os.environ['DB_USERNAME'],
+        password=os.environ['DB_PASSWORD'])
+    # create a cursor
+    cur = conn.cursor()
+    # execute query
+    cur.execute("SELECT * FROM patients WHERE id=%s", (id,))
+    # fetch the data from the table
+    row = cur.fetchone()
+    # print(row)
+    conn.close()
+    return render_template('patient.html', patient=row)
--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 
-# crear el modelo de la base de datos
+# objeto de la base de datos
 db = SQLAlchemy()
 
 # crear un modelo paciente, con los datos de nombre, fecha de nacimiento, signos vitales
@@ -14,12 +14,13 @@ class Paciente(db.Model):
     signos_vitales = db.Column(db.String(80), unique=False, nullable=False)
 
     # constructor
-    def __init__(self, employee_id, name, age, position):
-        self.employee_id = employee_id
-        self.name = name
-        self.age = age
-        self.position = position
+    def __init__(self, id, nombre, fecha_nacimiento, signos_vitales):
+        self.id = id
+        self.nombre = nombre
+        self.fecha_nacimiento = fecha_nacimiento
+        self.signos_vitales = signos_vitales
 
+    # representacion del objeto
     def __repr__(self):
-        return f"{self.name}:{self.employee_id}"
+        return f'Paciente {self.nombre}'
 
